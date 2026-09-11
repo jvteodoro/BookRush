c39935ab9dfd3ce11d88007e3b2b87bfadc65658
João Teodoro <joao.v.teodoro@usp.br>
2026-09-11 03:54:47 -0300

    fix: register Backstage signals services

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -1,6 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import signalsPlugin from '@backstage/plugin-signals/alpha';
 import { authModule } from './modules/auth';
 import { navModule } from './modules/nav';
 
-export default createApp({ features: [catalogPlugin, navModule, authModule] });
+export default createApp({
+  features: [catalogPlugin, signalsPlugin, navModule, authModule],
+});
--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -22,5 +22,6 @@ backend.add(import('@backstage/plugin-search-backend-module-pg'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 backend.add(import('@backstage/plugin-user-settings-backend'));
+backend.add(import('@backstage/plugin-signals-backend'));
 
 backend.start();
